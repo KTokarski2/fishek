c0860c8101e408c1aa42358c434ba8024bada03d
KTokarski2 <krzysztof.tokarski2@icloud.com>
2026-02-11 19:45:52 +0100

    initial commit

---
+++ fishek-api/src/test/java/com/fishek/fishek_api/FishekApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishek.fishek_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishekApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
